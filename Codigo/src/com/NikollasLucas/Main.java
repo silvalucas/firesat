c75116da418f08d18363a3eecdf0d837bdb62c33
Nikollas Ferreira Gonçalves <nikollasferreirag@gmail.com>
2019-09-11 00:12:00 -0300

    Leitura arquivo ppm

---
+++ Codigo/src/com/NikollasLucas/Main.java
@@ -0,0 +1,12 @@
+package com.NikollasLucas;
+
+import Modelo.ImagemPpm;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ImagemPpm img = new ImagemPpm();
+        img.MontaClasse();
+
+    }
+}

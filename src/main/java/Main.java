f1f8f271b85b4b01dd58b37bceda6468655e42ec
Nikollas Ferreira Gonçalves <nikollasferreirag@gmail.com>
2019-09-16 21:34:34 -0300

    Modularização do código inciada e troca de conta google

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import Controle.ImagemControle;
+import Modelo.Imagem;
 import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.*;
@@ -6,8 +8,8 @@ import com.google.cloud.storage.Blob;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
-import com.google.firebase.cloud.StorageClient;
 
+import javax.rmi.CORBA.Util;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,33 +18,17 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
+import Util.UtilFirebase;
 
 public class Main {
     public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
-        System.out.println("Olha mundo");
-        /*FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.getApplicationDefault())
-                .build();
 
-        FirebaseApp.initializeApp(options);*/
-        InputStream serviceAccount = new FileInputStream("serviceAccount.json");
-        GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
-
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(credentials)
-                .setStorageBucket("firesat-3983a.appspot.com")
-                .build();
-        FirebaseApp app = FirebaseApp.initializeApp(options);
-
-        Firestore db = FirestoreClient.getFirestore();
-        DocumentReference docRef = db.collection("usuarios").document("NikFG");
-        Map<String, Object> data = new HashMap<>();
-        data.put("Nome", "Nikollas");
-        data.put("senha", "xxx");
-        data.put("ano", 1997);
-        ApiFuture<WriteResult> result = docRef.set(data);
-        ApiFuture<QuerySnapshot> query =
+        UtilFirebase utilFirebase = new UtilFirebase();
+        ImagemControle controle = new ImagemControle();
+        Imagem img[][] = controle.MontaMatriz("lagoa.ppm");
+        utilFirebase.GravaDadosImagem(img);
+        utilFirebase.PegaDadosImagem("Arquivo.json");
+        /*ApiFuture<QuerySnapshot> query =
                 db.collection("usuarios").get();
         QuerySnapshot querySnapshot = query.get();
         List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
@@ -63,7 +49,7 @@ public class Main {
         InputStream testFile = new FileInputStream("la.json");
         System.out.println(testFile.toString());
         //Blob blob = storage.create(blobInfo, testFile.toString().getBytes(UTF_8));
-        storage.create(blobInfo,testFile);
+        storage.create(blobInfo, testFile);*/
 
 
         //String blobString = "Diax/" + "la.txt";
---
+++ src/main/java/Util/UtilFirebase.java
@@ -0,0 +1,120 @@
+package Util;
+
+import Modelo.Imagem;
+import com.google.api.core.ApiFuture;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.WriteResult;
+import com.google.cloud.storage.*;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UtilFirebase {
+    public static Firestore db;
+    public static GoogleCredentials credentials;
+    private static FirebaseOptions options;
+
+    private static void getCredentials() throws IOException {
+        InputStream serviceAccount = new FileInputStream("serviceAccount.json");
+        credentials = GoogleCredentials.fromStream(serviceAccount);
+
+    }
+
+    private static void getInstance() throws IOException {
+        options = new FirebaseOptions.Builder()
+                .setCredentials(credentials)
+                .setStorageBucket("fire-sat.appspot.com")
+                .build();
+        FirebaseApp app = FirebaseApp.initializeApp(options);
+        db = FirestoreClient.getFirestore();
+    }
+
+    public UtilFirebase() {
+        try {
+            getCredentials();
+            getInstance();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void GravaDadosImagem(Imagem img[][]) {
+        final String arquivo = "Arquivo.json";
+        //imagem para json
+        Gson gson = new Gson();
+        String json = gson.toJson(img);
+        StorageOptions storageOptions = StorageOptions.newBuilder()
+                .setCredentials(credentials)
+                .build();
+        Storage storage = storageOptions.getService();
+        BlobId blobId = BlobId.of(options.getStorageBucket(), arquivo);
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("application/json").build();
+
+        try {
+            //Criando Json
+            FileWriter writer = new FileWriter(arquivo);
+            writer.write(json);
+            writer.close();
+
+            //salvando no firebase
+            InputStream file = new FileInputStream(arquivo);
+
+            storage.create(blobInfo, file);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        DocumentReference docRef = db.collection("imagens").document();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy ");
+        Date currentDate = new Date();
+        Map<String, Object> data = new HashMap<>();
+
+        data.put("imagem", arquivo);
+        data.put("data", formatter.format(currentDate));
+        data.put("baixada", false);
+        ApiFuture<WriteResult> result = docRef.set(data);
+
+       /* for (int i = 0; i < img.length; i++) {
+            for (int j = 0; j < img[i].length; j++) {
+                data.put("red", img[i][j].getRed());
+                data.put("green", img[i][j].getGreen());
+                data.put("blue", img[i][j].getBlue());
+                docRef.set(data);
+                docRef = db.collection("imagens").document("Cor1").collection("Cores").document(i + " " + j);
+            }*/
+
+
+    }
+
+
+    // if (result.isDone()) {
+    //add retorno
+    // }//add else com retorno
+
+
+    public void PegaDadosImagem(String nome) {
+        StorageOptions storageOptions = StorageOptions.newBuilder()
+                .setCredentials(credentials)
+                .build();
+        Storage storage = storageOptions.getService();
+        Blob blob = storage.get(BlobId.of(options.getStorageBucket(), nome));
+        Path destFilePath = Paths.get("D:/Games/Arquivo.json");
+        blob.downloadTo(destFilePath);
+
+    }
+    
+}

19e9630f9335fb62a792ab05790db98f8da41997
Lucas Oliveira <l_oliveiras@hotmail.com>
2019-10-09 14:45:57 -0300

    Alteração na tela de Login

--- src/main/java/LoginController.java
+++ src/main/java/LoginController.java
@@ -1,5 +1,7 @@
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
@@ -12,13 +14,42 @@ public class LoginController {
     @FXML
     private PasswordField txtPassword;
 
+    @FXML
+    private RadioButton radioAnalise;
+
+    @FXML
+    private RadioButton radioSatelite;
+
+    @FXML
+    private Label errorMsg;
+
     public void btnEntrar(javafx.event.ActionEvent actionEvent) throws IOException {
         String user = txtUser.getText();
         String password = txtPassword.getText();
         txtPassword.setText("");
         txtUser.setText("");
-        //verificar usuário e senha
-        Main.changeScreen("home");
+
+        //VERIFICAR LOGIN AQUI
+
+        boolean analise = radioAnalise.isSelected();
+        boolean satelite = radioSatelite.isSelected();
+
+        if(!analise && !satelite){
+            errorMsg.setText("SELECIONE UM DOS MODULOS!");
+        } else if(satelite){
+            errorMsg.setText("");
+            Main.changeScreen("homeSatelite");
+        } else{
+            errorMsg.setText("");
+            Main.changeScreen("home");
+        }
     }
 
+    public void limpaSatelite(javafx.event.ActionEvent actionEvent){
+        radioSatelite.setSelected(false);
+    }
+
+    public void limpaAnalise(javafx.event.ActionEvent actionEvent){
+        radioAnalise.setSelected(false);
+    }
 }

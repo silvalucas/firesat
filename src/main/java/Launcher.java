6df6119eaddd30b82fe5b9ec3072699391d7f640
Nikollas Ferreira Gonçalves <nikollasferreirag@gmail.com>
2019-10-16 20:34:35 -0300

    .

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,10 @@
+import Main.Main;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+public class Launcher {
+    public static void main(String[] args) throws InterruptedException, ExecutionException, IOException {
+        Main.main(args);
+    }
+}
